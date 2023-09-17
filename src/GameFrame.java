d26c8e5008ca5b990d57e71c7e2c10a40099dbf3
Layzerg <Layzerg@Layzerg>
2023-09-17 14:41:02 +0300

    1

---
+++ src/GameFrame.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+
+	GamePanel panel;
+	
+	
+	GameFrame(){
+		panel= new GamePanel();
+		this.add(panel);
+		this.setTitle("PONGGGG");
+		this.setResizable(false);
+		this.setBackground(Color.black);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.pack();
+		this.setLocationRelativeTo(null);
+		
+		
+		this.setVisible(true);
+		
+		
+	}
+	
+	}
